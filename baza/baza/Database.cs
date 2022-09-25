fa88818a96f3c4099d4b30ea90e7cc5a57ae289e
pordo <pordo@TUF-DASH-F15>
2022-09-25 20:47:04 +0200

    Dodaj pliki projektów.

---
+++ baza/baza/Database.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace baza
+{
+    public class Database
+    {
+        private readonly SQLiteAsyncConnection _database;
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Person>();
+        }
+
+        public List<Person> GetPeople()
+        {
+            List<baza.Person> Person = _database.Table<Person>().ToListAsync().Result;
+            if(Person.Count > 0)
+                Person[0].Name += " to pierwsza zapisana w bazie osoba";
+            Person.Add(new Person { Name = "Ostatnia osoba której nie ma w bazie", Subscribed = true });
+            foreach (Person person in Person)
+            {
+                if (person.Subscribed == true)
+                    person.Status = "Subskrybent";
+                else
+                    person.Status = "Jeszcze nie jest subsrybentem";
+            }
+            return Person;
+        }
+
+        public Task<List<Person>> GetPeopleAsync()
+        {
+            var result = Task.Run(() => GetPeople());
+            return result;
+        }
+
+        /*
+        public Task<List<Person>> GetPeopleAsync()
+        {
+            return _database.Table<Person>().ToListAsync();
+        }
+         */
+
+        public Task<int> SavePersonAsync(Person person)
+        {
+            return _database.InsertAsync(person);
+        }
+
+        public Task DeleteAllItems()
+        {
+            return _database.DeleteAllAsync<Person>();
+        }
+    }
+}
---
+++ baza/baza/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace baza
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // ////
+        async Task Load()
+        {
+            List<Person> Person = await App.Database.GetPeopleAsync();
+            foreach (Person person in Person)
+            {
+                person.Name = char.ToUpper(person.Name[0]).ToString() + person.Name.Substring(1);
+            }
+            collectionView.ItemsSource = Person;
+        }
+        // ///
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing(); // wymusza odświeżenie kolekcji
+            //collectionView.ItemsSource = await App.Database.GetPeopleAsync();
+
+            await Load();
+        }
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(nameEntry.Text))
+            {
+                await App.Database.SavePersonAsync(new Person
+                {
+                    Name = nameEntry.Text,
+                    Subscribed = subscribed.IsChecked
+                });
+                nameEntry.Text = string.Empty;
+                subscribed.IsChecked = false;
+                //collectionView.ItemsSource = await App.Database.GetPeopleAsync();
+
+                await Load();
+                string name = App.Database.GetPeopleAsync().Result[App.Database.GetPeopleAsync().Result.Count-2].Name;
+                lblO.Text = "Ostatnio dodana do bazy osoba to " + name;
+
+            }
+        }
+
+        // usuwanie wszystkiego z bazy
+        private async void btnClear_Clicked(object sender, EventArgs e)
+        {
+            await App.Database.DeleteAllItems();
+            await Load();
+        }
+    }
+    }
